d73d24eb600a9797921e2ac2c78c381675e22d05
RicoVianna <rluivianna@hotmail.com>
2026-03-21 20:41:45 -0300

    Finalizando a configuração do PWA com icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'contrato-facil-v1';
+const CACHE_NAME = 'contrato-facil-v2'; // Mudei para v2 para forçar atualização
 const assets = [
     './',
     './index.html',
@@ -9,16 +9,25 @@ const assets = [
     './icons/icon-512.png'
 ];
 
-// Instalação do Service Worker
+// Instalação: Salva arquivos no cache
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(assets);
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+    );
+});
+
+// Ativação: Limpa caches antigos (Importante para o botão de instalar!)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
         })
     );
 });
 
-// Responde às requisições
+// Busca: Tenta o cache primeiro, se não tiver, vai na rede
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
